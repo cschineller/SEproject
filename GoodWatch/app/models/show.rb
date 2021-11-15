119ea581435fbe03f844709ffd82dc8a3e818111
ggrossman618 <ggrossman@colgate.edu>
2021-11-15 16:41:07 -0500

    show model created and started validations

--- GoodWatch/app/models/show.rb
+++ GoodWatch/app/models/show.rb
@@ -1,2 +1,20 @@
 class Show < ApplicationRecord
+  has_many :reviews
+  has_and_belongs_to_many :producers
+  has_many :review_authors, :through => :reviews
+
+  validates :title, presence: true
+  validates :studio, presence: true
+  validates :number_of_seasons, numericality: {greater_than_or_equal_to: 0}
+  validates :year, numericality: {only_integer: true, greater_than_or_equal_to: 1000}
+  validates :pages, numericality: {only_integer: true, greater_than: 0}
+
+  after_save :methodname
+
+  def self.title_search(query)
+    Show.where('title LIKE ?', "%#{query}%")
+  end
+
+  private
+
 end
