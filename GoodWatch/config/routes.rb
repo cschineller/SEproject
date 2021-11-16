a716c9726be5cabaaddbe819f695cb294ac8a389
JDAmico09 <jake.damico.3@gmail.com>
2021-11-16 17:03:05 -0500

    Got an initial index page up

--- GoodWatch/config/routes.rb
+++ GoodWatch/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :shows, only: [:index, :show, :new, :create]
-  root 'shows#index'
+  root to: 'shows#index'
   devise_for :users
 end
