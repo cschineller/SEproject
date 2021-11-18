c08885ce4ad787ff8225b5c83f79f6f512411bd7
ggrossman618 <ggrossman@colgate.edu>
2021-11-18 16:20:16 -0500

    created create_review, coded it, and changed notes.txt file

---
+++ GoodWatch/db/migrate/20211118211749_create_review.rb
@@ -0,0 +1,12 @@
+class CreateReview < ActiveRecord::Migration[6.1]
+  def change
+    create_table :reviews do |t|
+      t.integer :stars
+      t.text :review
+      t.references :book, null: false, foreign_key: true
+      t.references :review_author, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
