1b539147ea2d7e5ac4dcf8564ac9dfa74b3e8e7c
ggrossman618 <ggrossman@colgate.edu>
2021-11-26 16:54:59 -0500

    fixed db migration (we all need to re-migrate), and created crud-spec and it's framework <-- NOT DONE

--- GoodWatch/db/migrate/20211118211749_create_review.rb
+++ GoodWatch/db/migrate/20211118211749_create_review.rb
@@ -3,7 +3,7 @@ class CreateReview < ActiveRecord::Migration[6.1]
     create_table :reviews do |t|
       t.integer :stars
       t.text :review
-      t.references :book, null: false, foreign_key: true
+      t.references :show, null: false, foreign_key: true
       t.references :review_author, foreign_key: true
 
       t.timestamps
---
+++ GoodWatch/spec/requests/crud_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe "CRUDRoutes", type: :request do
+    describe "go through index route" do
+    end
+
+    describe "go through show route" do
+    end
+
+    describe "go through new route" do
+    end
+
+    describe "go through create route" do
+    end
+
+    describe "go through destroy route" do
+    end
+end
