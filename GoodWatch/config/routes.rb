504977e62e180a816954d2982b787e8f90b5534b
ggrossman618 <ggrossman@colgate.edu>
2021-11-27 13:47:38 -0500

    created shows routes <-- would love someone to take a look at this

--- GoodWatch/config/routes.rb
+++ GoodWatch/config/routes.rb
@@ -3,4 +3,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :shows, only: [:index, :show, :new, :create]
   root to: 'shows#index'
+
+  #CRUD routes linking HTML path to shows_controller
+  get '/shows', to: 'shows#index'
+  get '/shows/:id', to: 'shows#show'
+  get '/shows/new', to:'shows#new'
+  get '/shows/edit', to: 'shows#edit'
+  post '/shows', to: 'shows#create'
+  patch '/shows/:id', to: 'shows#update'
+  delete '/shows/:id', to: '/shows#destroy'
+
 end
