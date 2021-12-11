c1a1f3a927c03f68843e099497f4e45ca7a12da9
ggrossman618 <ggrossman@colgate.edu>
2021-12-10 22:01:42 -0500

    added some more fun seeds

--- GoodWatch/db/seeds.rb
+++ GoodWatch/db/seeds.rb
@@ -15,6 +15,13 @@ b1 = Show.create!(title: "Community", creator: "Dan Harmon", number_of_seasons: 
 b2 = Show.create!(title: "Ozark", creator: "Bill Dubuque", number_of_seasons: 4, year: 2017, episodes: 30)
 b3 = Show.create!(title: "The Wire", creator: "David Simon", number_of_seasons: 5, year: 2002, episodes: 60)
 b4 = Show.create!(title: "Stranger Things", creator: "Duffer Brothers", number_of_seasons: 3, year: 2016, episodes: 25)
+b5 = Show.create!(title: "Breaking Bad", creator: "Vince Gilligan", number_of_seasons: 5, year: 2008, episodes: 62)
+b6 = Show.create!(title: "Planet Earth", creator: "George Fenton", number_of_seasons: 1, year: 2006, episodes: 11)
+b7 = Show.create!(title: "The Sopranos", creator: "David Chase", number_of_seasons: 6, year: 1999, episodes: 86)
+b8 = Show.create!(title: "Game of Thrones", creator: "David Benioff", number_of_seasons: 8, year: 2011, episodes: 73)
+b9 = Show.create!(title: "Sherlock", creator: "Mark Gatiss", number_of_seasons: 4, year: 2010, episodes: 15)
+b10 = Show.create!(title: "Monty Python's Flying Circus", creator: "Graham Chapman", number_of_seasons: 4, year: 1969, episodes: 45)
+
 
 #Seeded Review Authors
 ra1 = ReviewAuthor.create!(name: 'Bad Review 1', user_id: u1.id)
